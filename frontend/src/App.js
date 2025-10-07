f42b2506b7811ff6dd1a910707fccad6cfaf77bb
LowYingQi <lowyingqi121933@gmail.com>
2025-10-07 20:39:53 +0800

    Main Page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import MainPage from "./pages/MainPage";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CoursePage from "./pages/CoursePage";
+import RecipePage from "./pages/RecipePage";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/course" element={<CoursePage />} />
+        <Route path="/recipe" element={<RecipePage />} />
       </Routes>
     </Router>
   );
